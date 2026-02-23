984ce0ca6d03201220c6deed1c0b7444eeb91a2f
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2026-02-23 15:12:31 +0000

    Add skeleton Micro_C_Interfaces session
    Depends on Micro_Rust_Interfaces_Core, Separation_Lenses, and
    Shallow_Micro_C. Will host C block references, stack frame management,
    and deallocation.
    
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

---
+++ Micro_C_Interfaces/Micro_C_Interfaces.thy
@@ -0,0 +1,8 @@
+(* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT *)
+
+theory Micro_C_Interfaces
+  imports
+    "Shallow_Micro_C.Shallow_Micro_C"
+begin
+end
