046336027e275a78c364ecac22f909eaa1d12db8
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2026-02-23 15:13:39 +0000

    Verify Isabelle/C parser integration with trivial C file
    Parse a minimal main() and a swap() function via the Isabelle/C parser
    to confirm the C11 front-end loads and produces C_Ast output.
    
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

--- Micro_C_Parsing_Frontend/Micro_C_Syntax.thy
+++ Micro_C_Parsing_Frontend/Micro_C_Syntax.thy
@@ -6,4 +6,22 @@ theory Micro_C_Syntax
     "Isabelle_C.C_Main"
 begin
 
+section \<open>Isabelle/C Parser Integration\<close>
+
+text \<open>Verify that the Isabelle/C parser loads and can parse trivial C code.\<close>
+
+C \<open>
+int main(void) {
+  return 0;
+}
+\<close>
+
+C \<open>
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+\<close>
+
 end
