4d13e27ad999556810a03e3d2e0a65055709061e
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2026-02-23 15:12:13 +0000

    Add skeleton Micro_C_Parsing_Frontend session
    Imports Isabelle/C (C_Main) and Shallow_Micro_Rust. Contains an empty
    Micro_C_Syntax theory that will host the C parser integration.
    
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

---
+++ Micro_C_Parsing_Frontend/Micro_C_Parsing_Frontend.thy
@@ -0,0 +1,8 @@
+(* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT *)
+
+theory Micro_C_Parsing_Frontend
+  imports
+    Micro_C_Syntax
+begin
+end
---
+++ Micro_C_Parsing_Frontend/Micro_C_Syntax.thy
@@ -0,0 +1,9 @@
+(* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT *)
+
+theory Micro_C_Syntax
+  imports
+    "Isabelle_C.C_Main"
+begin
+
+end
