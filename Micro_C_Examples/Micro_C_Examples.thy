1f417b7e88e38ce811f1ff7534694f7e11869c8e
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2026-02-23 15:12:42 +0000

    Add skeleton Micro_C_Examples session and update umbrella
    Add Micro_C_Examples session depending on Micro_C_Interfaces and Crush.
    Update top-level ROOT and AutoCorrode.thy to include all four new
    Micro C sessions in the umbrella build.
    
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

---
+++ Micro_C_Examples/Micro_C_Examples.thy
@@ -0,0 +1,8 @@
+(* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT *)
+
+theory Micro_C_Examples
+  imports
+    "Micro_C_Interfaces.Micro_C_Interfaces"
+begin
+end
