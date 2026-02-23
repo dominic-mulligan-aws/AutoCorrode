9526a3cadeb4e6fbf98a9678124a34112c4de229
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2026-02-23 15:12:21 +0000

    Add skeleton Shallow_Micro_C session
    Depends on Shallow_Micro_Rust, Shallow_Separation_Logic, and
    Byte_Level_Encoding. Will host C numeric types, abort types, pointer
    types, and memory operations.
    
    Signed-off-by: Dominic Mulligan <dominic.p.mulligan@gmail.com>

---
+++ Shallow_Micro_C/Shallow_Micro_C.thy
@@ -0,0 +1,8 @@
+(* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT *)
+
+theory Shallow_Micro_C
+  imports
+    "Shallow_Micro_Rust.Core_Expression"
+begin
+end
